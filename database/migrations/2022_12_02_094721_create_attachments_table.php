559b02d630c0fa2dbe292c4e5156378e6e8168f1
saisarah <sarahoben7@gmail.com>
2022-12-02 17:52:56 +0800

    Add attachments tbl

---
+++ database/migrations/2022_12_02_094721_create_attachments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attachments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('post_id');
+            $table->string('source', 255);
+            $table->string('type', 10);
+            $table->foreign('post_id')->references('id')->on('post');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attachments');
+    }
+};
