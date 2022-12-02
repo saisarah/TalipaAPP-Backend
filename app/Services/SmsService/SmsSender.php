69294c3eeda720cef946e4082701868577745452
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2022-12-02 09:10:45 +0000

    Implement SMS OTP Verification

--- app/Services/SmsService/SmsSender.php
+++ app/Services/SmsService/SmsSender.php
@@ -4,32 +4,11 @@ namespace App\Services\SmsService;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static void sendMessage($to, $message)
+ */
 class SmsSender extends Facade
 {
-    private $driver;
-
-    // protected static $drivers = [
-    //     'movider' => SmsMoviderAdapter::class,
-    //     'log' => SmsLogAdapter::class,
-    // ];
-
-    // public function __construct()
-    // {
-    //     $this->driver = config('sms.default');
-    // }
-
-    // public function driver($driver)
-    // {
-    //     $this->driver = $driver;
-    //     return $this;
-    // }
-
-    // public function sendMessage($to, $message)
-    // {
-    //     $sender = app()->make($this->drivers[$this->driver]);
-    //     return $sender->sendMessage($to, $message);
-    // }
-
     protected static function getFacadeAccessor()
     {
         return 'sms';        
--- config/app.php
+++ config/app.php
@@ -211,6 +211,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'SmsOtp' => App\Services\SmsService\SmsOtp\SmsOtp::class,
     ])->toArray(),
 
 ];
