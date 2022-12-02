bc80d9511a9a945676b1a2b8b3174094c9e81ae4
saisarah <sarahoben7@gmail.com>
2022-12-02 18:48:58 +0800

    Update vendor crops tbl

---
+++ database/migrations/2022_12_02_104832_create_vendor_crops_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendor_crops', function (Blueprint $table) {
+            $table->unsignedBigInteger('vendor_id');
+            $table->unsignedBigInteger('crop_id');
+            $table->timestamps();
+            $table->foreign('vendor_id')->references('user_id')->on('vendors');
+            $table->foreign('crop_id')->references('id')->on('crops');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendor_crops');
+    }
+};
