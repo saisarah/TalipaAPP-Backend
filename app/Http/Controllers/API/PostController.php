78a8ca094d9177465ad2e9d53f8f378d911a0c52
Uno-count <paonemorfi@gmail.com>
2022-12-09 08:24:24 +0000

    Fetch  Posts with authors

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -10,9 +10,11 @@ class PostController extends Controller
 {
    public function index(Request $request)
    {
-      if ($request->crop === null)
-         return Post::paginate(10);
-
-      return Post::where('crop_id', $request->crop)->paginate(10);        
+      return Post::query()
+         ->with('author')
+         ->when($request->crop !== null, function ($query) use ($request) {
+            $query->where('crop_id', $request->crop);
+           })
+         ->paginate(10);
    }
 }
