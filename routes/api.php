8eb354f58507f56cf0e59cbb703800d961acee8d
Uno-count <paonemorfi@gmail.com>
2022-12-09 04:59:29 +0000

    fetch-backend

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\API\Auth\FarmerRegisterController;
 use App\Http\Controllers\API\AddressController;
 use App\Http\Controllers\API\Auth\LoginController;
 use App\Http\Controllers\API\Auth\VendorRegisterController;
+use App\Http\Controllers\API\CropController;
+use App\Http\Controllers\API\PostController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +21,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::post('/login/send-otp', [LoginController::class, 'sendOtp']);
 Route::post('/login/verify-otp', [LoginController::class, 'verifyOtp']);
@@ -28,8 +28,12 @@ Route::post('/login/verify-otp', [LoginController::class, 'verifyOtp']);
 Route::post('/register/vendor', [VendorRegisterController::class, 'register']);
 Route::post('/register/farmer', [FarmerRegisterController::class, 'register']);
 
+
 Route::get('/philippine-addresses/regions', [AddressController::class, 'regions']);
 Route::get('/philippine-addresses/provinces', [AddressController::class, 'provinces']);
 Route::get('/philippine-addresses/cities', [AddressController::class, 'cities']);
 Route::get('/philippine-addresses/barangays', [AddressController::class, 'barangays']);
+Route::get('/posts', [PostController::class,'index']);
+Route::get('/crops',[CropController::class,'index']);
+Route::get('/user',[UserController::class,'getCurrentUser'])->middleware('auth:sanctum');
 
