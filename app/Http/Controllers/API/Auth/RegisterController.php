6971c6422a0c39c787676b3ec21d618a8ae40021
saisarah <sarahoben7@gmail.com>
2022-12-11 10:28:05 +0800

    Refactor Register Controller

--- app/Http/Controllers/API/Auth/RegisterController.php
+++ app/Http/Controllers/API/Auth/RegisterController.php
@@ -3,15 +3,41 @@
 namespace App\Http\Controllers\API\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\FarmerRegisterRequest;
 use App\Http\Requests\Auth\VendorRegisterRequest;
 use App\Models\Address;
+use App\Models\Farmer;
 use App\Models\User;
 use App\Models\Vendor;
 use Illuminate\Http\Request;
 
-class VendorRegisterController extends Controller
+class RegisterController extends Controller
 {
-    public function register(VendorRegisterRequest $request)
+    public function registerFarmer(FarmerRegisterRequest $request)
+    {
+        $user = new User();
+        $user->firstname = $request->firstname;
+        $user->middlename = $request->middlename;
+        $user->lastname = $request->lastname;
+        $user->contact_number = $request->contact_number;
+        $user->email = $request->email;
+        $user->user_type = User::TYPE_FARMER;
+        $user->gender = $request->gender;
+        $user->profile_picture = $request->profile_picture;
+        $user->save();
+
+
+        $farmer = new Farmer();
+        $farmer->user_id = $user->id;
+        $farmer->farm_area = $request->farm_area;
+        $farmer->farm_type = $request->farm_type;
+        $farmer->ownership_type = $request->ownership_type;
+        $farmer->save();
+
+        return $user;
+    }
+
+    public function registerVendor(VendorRegisterRequest $request)
     {
         $user = new User();
         $user->firstname = $request->firstname;
@@ -42,5 +68,15 @@ class VendorRegisterController extends Controller
 
 
         return $user;
+    }
+
+    public function validator(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'sometimes|email|unique:users',
+            'phone' => 'sometimes|digits:10|numeric|unique:users,contact_number'
+        ]);
+
+        return true;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,11 @@
 <?php
 
-use App\Http\Controllers\API\Auth\FarmerRegisterController;
 use App\Http\Controllers\API\AddressController;
 use App\Http\Controllers\API\Auth\LoginController;
-use App\Http\Controllers\API\Auth\VendorRegisterController;
+use App\Http\Controllers\API\Auth\RegisterController;
 use App\Http\Controllers\API\CropController;
 use App\Http\Controllers\API\PostController;
 use App\Http\Controllers\API\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,16 +23,16 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login/send-otp', [LoginController::class, 'sendOtp']);
 Route::post('/login/verify-otp', [LoginController::class, 'verifyOtp']);
 
-Route::post('/register/vendor', [VendorRegisterController::class, 'register']);
-Route::post('/register/farmer', [FarmerRegisterController::class, 'register']);
-
+Route::post('/register/vendor', [RegisterController::class, 'registerFarmer']);
+Route::post('/register/farmer', [RegisterController::class, 'registerVendor']);
+Route::post('/register/validator', [RegisterController::class, 'validator']);
 
 Route::get('/philippine-addresses/regions', [AddressController::class, 'regions']);
 Route::get('/philippine-addresses/provinces', [AddressController::class, 'provinces']);
 Route::get('/philippine-addresses/cities', [AddressController::class, 'cities']);
 Route::get('/philippine-addresses/barangays', [AddressController::class, 'barangays']);
-Route::get('/posts', [PostController::class,'index'])->middleware('auth:sanctum');
-Route::get('/crops',[CropController::class,'index'])->middleware('auth:sanctum');
-Route::get('/user',[UserController::class,'getCurrentUser'])->middleware('auth:sanctum');
+Route::get('/posts', [PostController::class, 'index'])->middleware('auth:sanctum');
+Route::get('/crops', [CropController::class, 'index'])->middleware('auth:sanctum');
+Route::get('/user', [UserController::class, 'getCurrentUser'])->middleware('auth:sanctum');
 
 Route::post('/posts', [PostController::class, 'create'])->middleware('auth:sanctum');
