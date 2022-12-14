6aa7b4fa8f7af4867293525d02a58339359c2f7b
saisarah <sarahoben7@gmail.com>
2022-12-14 19:05:15 +0800

    Update Farm table columns

--- database/migrations/2022_11_30_060841_create_farmers_table.php
+++ database/migrations/2022_11_30_060841_create_farmers_table.php
@@ -15,9 +15,12 @@ return new class extends Migration
     {
         Schema::create('farmers', function (Blueprint $table) {
             $table->id('user_id');
-            $table->string('farm_area', 255);
+            $table->string('farm_area', 60);
             $table->string('farm_type', 60);
+            $table->string('land_owner', 100)->nullable();;
             $table->string('ownership_type', 60);
+            $table->string('document_type', 60);
+            $table->string('document', 255);
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users');
         });
