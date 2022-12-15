193b2c1605d643398604fd884f8a1dacb1c3b40d
Uno-count <paonemorfi@gmail.com>
2022-12-15 01:41:00 +0000

    edit Register OTP

--- app/Services/SmsService/SmsOtp/RegisterOtp.php
+++ app/Services/SmsService/SmsOtp/RegisterOtp.php
@@ -4,6 +4,6 @@ namespace App\Services\SmsService\SmsOtp;
 
 class RegisterOtp extends SmsOtp
 {
-    protected $message_template = "Thank you for signing up, here is your Talipaapp registration code OTP_CODE ";
+    protected $message_template = "Good Day! Thank you for signing up, to verify your account, please enter this OTP: OTP_CODE to continue using Talipaapp.";
     protected $type = "register";
 }
