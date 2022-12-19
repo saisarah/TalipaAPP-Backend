526932c615f96f8490cc1040a0c244511d3a3197
lenard123 <lenard.mangayayam@gmail.com>
2022-12-19 23:54:48 +0000

    Fix

--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Attachment extends Model
 {
     use HasFactory;
+
+    public function source() : Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => url(Storage::url($value))
+        );
+    }
 }
