c3a982f1250d78401b8206ff40970acbed652c95
saisarah <sarahoben7@gmail.com>
2023-01-31 22:50:46 +0800

    Add demands table

---
+++ database/migrations/2023_01_31_144439_create_demands_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('demands', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('vendor_id');
+            $table->unsignedDecimal('budget');
+            $table->unsignedBigInteger('quantity');
+            $table->string('description', 2000);
+            $table->unsignedBigInteger('crop_id');
+            $table->foreign('vendor_id')->references('user_id')->on('vendors');
+            $table->foreign('crop_id')->references('id')->on('crops');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('demands');
+    }
+};
