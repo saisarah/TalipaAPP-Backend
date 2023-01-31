a705a64ff28465693f8d8b8996bf8555cc94690d
saisarah <sarahoben7@gmail.com>
2023-01-31 22:25:56 +0800

    Fetch specific group

--- routes/api.php
+++ routes/api.php
@@ -47,6 +47,7 @@ Route::post('/posts', [PostController::class, 'create'])->middleware('auth:sanct
 Route::get('/users/{user}/posts', [PostController::class, 'getFromUser'])->middleware('auth:sanctum');
 
 Route::get('/farmer-groups', [FarmerGroupController::class, 'index']);
+Route::get('/farmer-groups/{id}', [FarmerGroupController::class, 'show'])->middleware('auth:sanctum');
 Route::get('/farmers', [FarmerController::class, 'index']);
 Route::get('/farmer-group-posts', [FarmerGroupPostController::class, 'index'])->middleware('auth:sanctum');
 
