a5c3189ac3ab2b6e700691a46668a2ad803213eb
saisarah <sarahoben7@gmail.com>
2023-01-31 21:05:37 +0800

    Fetch group post

--- app/Http/Controllers/API/FarmerGroupPostController.php
+++ app/Http/Controllers/API/FarmerGroupPostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\FarmerGroupMember;
 use App\Models\FarmerGroupPost;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,6 +12,14 @@ class FarmerGroupPostController extends Controller
 {
     public function index()
     {
-        return $id = Auth::id();
+        $id = Auth::id();
+        $group = FarmerGroupMember::where('farmer_id', $id)->first();
+        if ($group == null)
+        {
+            abort(400, "Join Group to view Discussions");
+        }
+        $group_id = $group->farmer_group_id;
+        $group_posts = FarmerGroupPost::where('farmer_group_id', $group_id)->get();
+        return $group_posts;
     }
 }
