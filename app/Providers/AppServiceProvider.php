0bdb3f18e2a08afe01a390c10547739cff5906d1
lenard123 <lenard.mangayayam@gmail.com>
2023-02-01 17:51:33 +0000

    Fix CORS Error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: *');
+        header('Access-Control-Allow-Headers: *');
     }
 }
