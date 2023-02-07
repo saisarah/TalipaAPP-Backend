5e1c6e7ff145c83c92f4aa94fbfcfc72ddf14949
saisarah <sarahoben7@gmail.com>
2023-02-07 16:27:47 +0800

    Create Demand Seeder

---
+++ app/Models/Demand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Demand extends Model
+{
+    use HasFactory;
+
+    public function author()
+    {
+        return $this->belongsTo(Vendor::class, 'vendor_id', 'user_id'); 
+    }
+
+    public function crop()
+    {
+        return $this->belongsTo(Crop::class, 'crop_id');
+    }
+}
