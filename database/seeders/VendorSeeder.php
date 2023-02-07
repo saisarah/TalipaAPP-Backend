0e62bd354e2b3176b8a88aea41c475da131c7e9d
saisarah <sarahoben7@gmail.com>
2023-02-07 16:26:30 +0800

    Create Vendor seeder

---
+++ database/seeders/VendorSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Vendor;
+
+class VendorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Vendor::factory()->count(100)->create();
+    }
+}
