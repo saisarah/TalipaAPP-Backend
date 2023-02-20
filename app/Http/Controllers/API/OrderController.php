3bd38f0a1d631cac914fbc7604510b1940a9a139
saisarah <sarahoben7@gmail.com>
2023-02-20 09:29:51 +0000

    Fetch all orders

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -3,14 +3,30 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Models\Orders;
+use App\Models\Order;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $orders = Orders::all();
-        return $orders;
+        $user = Auth::user();
+
+        if ($user->isFarmer()) {
+            $orders = Order::whereHas('post', function ($q) {
+                $id = Auth::id();
+                $q->where('author_id', $id);
+            })->where('order_status', $request->status)->get();
+            return  $orders;
+        } else {
+            $id = Auth::id();
+            $orders = Order::where('buyer_id', $id)
+                ->where('order_status', $request->status)
+                ->get();
+            return  $orders;
+        }
     }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Orders extends Model
+class Order extends Model
 {
     use HasFactory;
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
