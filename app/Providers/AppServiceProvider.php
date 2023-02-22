8fcf71e53fa4f5bf69199b0b356744fc62268bbd
lenard123 <lenard.mangayayam@gmail.com>
2023-02-22 07:10:50 +0000

    Remove Access-Control-Allow headers from AppServiceProvider and update cors paths to allow all paths

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: *');
-        header('Access-Control-Allow-Headers: *');
     }
 }
