e8c14cee2a0c04443b8a124770e341ce885d60fc
lenard123 <lenard.mangayayam@gmail.com>
2023-02-23 16:06:32 +0000

    Add getRandomCrop() method to Crop model

--- app/Models/Crop.php
+++ app/Models/Crop.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Crop extends Model
 {
     use HasFactory;
+
+    private static ?Collection $crops = null;
+
+    public static function getRandomCrop()
+    {
+        if (is_null(static::$crops)) {
+            static::$crops = Crop::all();
+        }
+
+        return static::$crops->random()->id;
+    }
 }
