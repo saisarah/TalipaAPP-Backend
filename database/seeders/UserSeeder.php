55857df75b3a5f8a450116087cd06ee15615afa9
lenard123 <lenard.mangayayam@gmail.com>
2023-02-23 16:07:24 +0000

    Add Farmer and Vendor factories to UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Farmer;
 use App\Models\User;
+use App\Models\Vendor;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,18 +17,24 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->farmer()->create([
-            'firstname' => env('USER_FIRSTNAME', 'John'),
-            'lastname' => env('USER_LASTNAME', 'Doe'),
-            'email' => env('USER_EMAIL', 'john@doe.com'),
-            'contact_number' => env('USER_PHONE', '9123456789'),
-        ]);
+        User::factory()
+            ->farmer()
+            ->has(Farmer::factory())
+            ->create([
+                'firstname' => env('USER_FIRSTNAME', 'John'),
+                'lastname' => env('USER_LASTNAME', 'Doe'),
+                'email' => env('USER_EMAIL', 'john@doe.com'),
+                'contact_number' => env('USER_PHONE', '9123456789'),
+            ]);
 
-        User::factory()->vendor()->create([
-            'firstname' => 'Sarah',
-            'lastname' => 'Oben',
-            'email' => 'sarah@email.com',
-            'contact_number' => '9876543210',
-        ]);
+        User::factory()
+            ->vendor()
+            ->has(Vendor::factory())
+            ->create([
+                'firstname' => 'Sarah',
+                'lastname' => 'Oben',
+                'email' => 'sarah@email.com',
+                'contact_number' => '9876543210',
+            ]);
     }
 }
