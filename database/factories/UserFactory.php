ecd0bbda203549a3b516115a8a9ab9c4fb22f94f
saisarah <sarahoben7@gmail.com>
2023-02-28 01:08:18 +0800

    Update UserFactory to remove username field

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,7 +29,6 @@ class UserFactory extends Factory
             'user_type' => fake()->randomElement([User::TYPE_FARMER, User::TYPE_VENDOR, User::TYPE_ADMIN]),
             'password' => '$2y$10$Oe5z.q8KJrGXLkPPhSuaTuLNSvQdh07k5DizCU3QmhZ9aHDpkI/5u', //bcrypt('Juan23'),
             'remember_token' => Str::random(10),
-            'username' => User::generateUserName($firstname),
         ];
     }
 
