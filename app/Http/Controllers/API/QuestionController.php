d93d213e017dc907eccb12d9d5eda818a505b2da
saisarah <sarahoben7@gmail.com>
2023-02-28 16:31:55 +0800

    Add index method to QuestionController to retrieve all questions

--- app/Http/Controllers/API/QuestionController.php
+++ app/Http/Controllers/API/QuestionController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Question;
 use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    //
+    public function index()
+    {
+        return Question::all();
+    }
 }
