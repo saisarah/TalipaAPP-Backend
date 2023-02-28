370296e4760a466f07b49d3df6faff005d5d8206
saisarah <sarahoben7@gmail.com>
2023-02-28 16:50:25 +0800

    Add Auth user ID and create question validation

--- app/Http/Controllers/API/QuestionController.php
+++ app/Http/Controllers/API/QuestionController.php
@@ -5,11 +5,28 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Question;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class QuestionController extends Controller
 {
     public function index()
     {
         return Question::all();
+    }
+    
+    public function create(Request $request)
+    {
+        $this->validate($request, [
+            'question' => 'required',
+            'answer' => 'required'
+        ]);
+
+        $faqs = new Question();
+        $faqs->user_id = Auth::id(); 
+        $faqs->question = $request->question;
+        $faqs->answer = $request->answer;
+        $faqs->save();
+
+        return $faqs;
     }
 }
