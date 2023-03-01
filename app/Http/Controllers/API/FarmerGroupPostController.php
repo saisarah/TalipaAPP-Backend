d8096d0e0f5200633713c029cdacd59db5806c44
saisarah <sarahoben7@gmail.com>
2023-03-01 12:25:54 +0800

    Add validation and create method for FarmerGroupPostController

--- app/Http/Controllers/API/FarmerGroupPostController.php
+++ app/Http/Controllers/API/FarmerGroupPostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\FarmerGroupMember;
 use App\Models\FarmerGroupPost;
+use App\Models\FarmerGroupPostImage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -21,5 +22,26 @@ class FarmerGroupPostController extends Controller
         $group_id = $group->farmer_group_id;
         $group_posts = FarmerGroupPost::where('farmer_group_id', $group_id)->get();
         return $group_posts;
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, [
+            'farmer_id' => 'required|exists:farmers,user_id',
+            'farmer_group_id' => 'required|exists:farmer_group_members,farmer_id',
+            'title' => 'required',
+            'description' => 'required',
+            'tags' => 'required',
+        ]);
+
+        $discussion = new FarmerGroupPost();
+        $discussion->farmer_id = Auth::id();
+        $discussion->farmer_group_id = $request->farmer_group_id;
+        $discussion->title = $request->title;
+        $discussion->description = $request->description;
+        $discussion->tags = $request->tags;
+        $discussion->save();
+
+        return $discussion;
     }
 }
