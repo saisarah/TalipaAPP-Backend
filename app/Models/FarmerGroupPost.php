39a5917e4037cd2831f59fc26ea085c615b9ebd1
saisarah <sarahoben7@gmail.com>
2023-03-01 12:26:17 +0800

    Update FarmerGroupPost model to include fillable fields

--- app/Models/FarmerGroupPost.php
+++ app/Models/FarmerGroupPost.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class FarmerGroupPost extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'tags'
+    ];
 }
