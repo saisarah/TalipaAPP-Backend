d512b996992ba217954945e2a42e59327584045c
saisarah <sarahoben7@gmail.com>
2023-03-01 09:48:45 +0800

    Add create route for Demands with auth middleware

--- routes/api.php
+++ routes/api.php
@@ -71,6 +71,8 @@ Route::get('/farmer-group', [FarmerGroupController::class, 'getCurrentGroup'])->
 Route::get('/vendors', [VendorController::class, 'index']);
 
 Route::get('/demands', [DemandController::class, 'index']);
+Route::get('/demands', [DemandController::class, 'create'])->middleware('auth:sanctum');
+
 
 Route::get('/messages', [MessageController::class, 'index'])->middleware('auth:sanctum');
 Route::get('/messages/{id}', [MessageController::class, 'show'])->middleware('auth:sanctum');
