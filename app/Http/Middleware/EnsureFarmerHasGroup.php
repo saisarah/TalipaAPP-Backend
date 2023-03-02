f784cd1be6cd674e0a8a6e28660920243ca277cc
saisarah <sarahoben7@gmail.com>
2023-03-02 17:07:04 +0800

    Update logic to ensure farmer has an approved group before proceeding

--- app/Http/Middleware/EnsureFarmerHasGroup.php
+++ app/Http/Middleware/EnsureFarmerHasGroup.php
@@ -19,8 +19,8 @@ class EnsureFarmerHasGroup
     public function handle(Request $request, Closure $next)
     {
         $id = Auth::id();
-        $has_group = FarmerGroupMember::where('farmer_id', $id)->exists();
-        if ($has_group) {
+        $has_group = FarmerGroupMember::where('farmer_id', $id)->first();
+        if ($has_group && $has_group->isApproved()) {
             return $next($request);
         }
 
