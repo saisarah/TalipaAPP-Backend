e86f11befd19e96a14115f48dc3b1d5a2742c91e
saisarah <sarahoben7@gmail.com>
2023-03-09 15:42:59 +0800

    Add fillable fields and status constants to FarmerGroup model

--- app/Models/FarmerGroup.php
+++ app/Models/FarmerGroup.php
@@ -9,6 +9,22 @@ class FarmerGroup extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'address',
+        'type',
+        'year_founded',
+        'authorization',
+        'group_description',
+        'contact_no',
+        'email',
+        'status'
+    ];
+
+    const STATUS_VERIFIED = 'verified';
+    const STATUS_PENDING = 'pending';
+
+
     public function members()
     {
         return $this->hasMany(FarmerGroupMember::class)->where('membership_status', 'approved');
@@ -29,4 +45,3 @@ class FarmerGroup extends Model
         return $this->hasOne(FarmerGroupMember::class)->where('role', 'president');
     }
 }
-
