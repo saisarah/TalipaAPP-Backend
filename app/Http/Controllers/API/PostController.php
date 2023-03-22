b8bc602d3f242824c3e8cfb7986c814c2ac74115
saisarah <sarahoben7@gmail.com>
2023-03-22 21:25:23 +0800

    Refactor PostController to include conditionally fetch posts based on crop_ids

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -52,9 +52,12 @@ class PostController extends Controller
 
     public function index(Request $request)
     {
-
-        $crop_id = explode(',', $request->crop_ids);
-        return Post::whereIn('crop_id', $crop_id)->with('thumbnail', 'author', 'prices', 'crop')->latest()->get()->each->append('display_price', 'location');
+        if ($request->has('crop_ids')) {
+            $crop_id = explode(',', $request->crop_ids);
+            return Post::whereIn('crop_id', $crop_id)->with('thumbnail', 'author', 'prices', 'crop')->latest()->get()->each->append('display_price', 'location');
+        } else {
+            return Post::with('thumbnail', 'author', 'prices', 'crop')->latest()->get()->each->append('display_price', 'location');
+        }
     }
 
     public function show(Post $post)
