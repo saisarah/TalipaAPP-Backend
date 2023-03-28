939edaa4651f53ddd40a1261b28d713f93b6eda4
saisarah <sarahoben7@gmail.com>
2023-03-29 00:40:33 +0800

    Update VendorController to return all Vendors instead of Farmers

--- app/Http/Controllers/API/VendorController.php
+++ app/Http/Controllers/API/VendorController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Farmer;
+use App\Models\Vendor;
 use Illuminate\Http\Request;
 
 class VendorController extends Controller
 {
     public function index()
     {
-        return Farmer::all();
+        return Vendor::all();
     }
 }
