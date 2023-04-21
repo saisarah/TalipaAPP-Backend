0569d6a3818bc87aff3bb3dd55749da36cf8b1d1
saisarah <sarahoben7@gmail.com>
2023-04-21 22:08:00 +0800

    Change method to get

--- routes/api.php
+++ routes/api.php
@@ -140,7 +140,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('/farmer-group/posts', 'index')->middleware('has_group');
         Route::post('/farmer-group/posts', 'create')->middleware('has_group');
         Route::post('/farmer-group-posts/{id}/comments', 'createComment')->middleware('farmer', 'has_group');
-        Route::post('/farmer-group-posts/{id}', 'show')->middleware('farmer', 'has_group');
+        Route::get('/farmer-group-posts/{id}', 'show')->middleware('farmer', 'has_group');
     });
 
     Route::controller(FarmerGroupMemberController::class)->group(function () {
